db5cf6aaa735aecebe18b7c184377df2d07d8d77
MatthijsvanMeeuwen <579720@student.fontys.nl>
2026-09-21 12:32:31 +0200

    Update DE_OPDRACHT.py

---
+++ DE_OPDRACHT.py
@@ -0,0 +1,53 @@
+import pandas as pd
+import numpy as np
+import random
+import math as m
+
+random.seed(42)
+
+df = pd.read_excel('PaintShop-September2026.xlsx')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
